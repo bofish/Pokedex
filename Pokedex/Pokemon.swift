e371a72adf7f3abe2ea73aee83866d6a253bd52a
Boyce <b0812en@gmail.com>
2017-05-23 22:46:47 +0200

    Pokemon class file

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,26 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Ben on 2017/5/23.
+//  Copyright © 2017年 Boyce. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _pokeID: Int!
+    private var _pokeName: String!
+    
+    var pokeID: Int {
+        
+        return _pokeID
+    }
+    
+    var pokeName: String {
+        
+        return _pokeName
+    }
+    
+}
